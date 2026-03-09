639ba47cd78722e3864fdf64e83aa682c76230a3
sourav <souravkumar18835@gmail.com>
2026-03-10 00:35:57 +0530

    feat(progress): enhance file reception with progress bar and size display

--- daemon/src/network.cpp
+++ daemon/src/network.cpp
@@ -9,6 +9,7 @@
 #include <fstream>  // for file writing
 #include <arpa/inet.h>  // provided functions to manipulate ip addresses
 #include <filesystem>   // to handle file sizes
+#include <iomanip>
 
 // Change this folder in case we change target folder
 std::string target_folder = "./litsync_folder/";
@@ -87,6 +88,20 @@ bool NetworkManager::startServer(int port){
 }
 
 
+std::string bytesToKBMB(long long bytes){
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(2);   // 2 decimal points
+    if(bytes >= 1024*1024){
+        double mb = static_cast<double>(bytes) / (1024.0 * 1024.0);
+        ss << mb << "MB";
+    }else{
+        double kb = static_cast<double>(bytes) / 1024.0;
+        ss << kb << "KB";
+    }
+    return ss.str();
+}
+
+
 void NetworkManager::listenForConnections(SyncManager& syncManager){
     struct sockaddr_in address;
     int addrLen = sizeof(address);
@@ -159,6 +174,7 @@ void NetworkManager::listenForConnections(SyncManager& syncManager){
                             total_received += initial_data_length;
                         }
 
+                        std::cout << "[Watcher] Receiving " << cmd.filename << std::endl;
                         // loop and download the rest of the file chunks
                         while(total_received < cmd.filesize){
                             int bytes_read = read(new_socket, buffer, sizeof(buffer));
@@ -171,14 +187,14 @@ void NetworkManager::listenForConnections(SyncManager& syncManager){
                             int bar_width = 50;
                             double progress = static_cast<double>(total_received) / cmd.filesize;
                             int pos = bar_width * progress;
-
-                            std::cout << "\r[Watcher] Receiving " << cmd.filename << " [";
+                            
+                            std::cout << "\r[";
                             for(int i=0; i<bar_width; i++){
                                 if(i < pos) std::cout << "=";
                                 else if(i == pos) std::cout << ">";
                                 else std::cout << " ";
                             }
-                            std::cout << "] " << int(progress *  100.0) << "%" << std::flush;
+                            std::cout << "] " << int(progress *  100.0) << "% " << bytesToKBMB(total_received) << " / " << bytesToKBMB(cmd.filesize) << ")" << std::flush;
                         }
 
                         std::cout << std::endl; // lock in the finished progress bar
